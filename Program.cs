fce0a9fc58cbe4ec32583263190d3025578b4883
wangand <wangand@onid.oregonstate.edu>
2020-05-01 22:32:45 -0700

    Working echo thingy

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace Dflat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true) {
+                Console.WriteLine("What is your name? Type 'exit' to exit");
+                string line = Console.ReadLine();
+
+                if (line == "exit") {
+                    Console.WriteLine("Goodbye");
+                    break;
+                }
+
+                string output = String.Format("Hello, {0}", line);
+                Console.WriteLine(output);
+
+            }
         }
     }
 }
