38c095cdac2d3ab4ff1ea10a601fdb4a275d9b33
wangand <wangand@onid.oregonstate.edu>
2020-05-02 23:10:13 -0700

    Part 2 complete

--- Program.cs
+++ Program.cs
@@ -2,22 +2,106 @@
 
 namespace Dflat
 {
+    enum StatementType { 
+        STATEMENT_INSERT,
+        STATEMENT_SELECT
+    }
+        enum MetaCommandResult
+    {
+        META_COMMAND_EXIT,
+        META_COMMAND_SUCCESS,
+        META_COMMAND_UNRECOGNIZED_COMMAND
+    }
+    enum PrepareResult
+    {
+        PREPARE_SUCCESS,
+        PREPARE_UNRECOGNIZED_STATEMENT
+    }
+    class Statement
+    {
+        public StatementType type;
+        public void ExecuteStatement() 
+        {
+            switch (this.type)
+            {
+                case StatementType.STATEMENT_INSERT:
+                    Console.WriteLine("This is where we would do an insert.");
+                    break;
+                case StatementType.STATEMENT_SELECT:
+                    Console.WriteLine("This is where we would do a select.");
+                    break;
+            }
+        }
+    }
     class Program
     {
+        static void PrintPrompt()
+        {
+            Console.Write("db > ");
+        }
+        
+        
+        static MetaCommandResult DoMetaCommand(string line)
+        {
+            if (line == ".exit")
+            {
+                return MetaCommandResult.META_COMMAND_EXIT;
+            }
+            else
+            {
+                return MetaCommandResult.META_COMMAND_UNRECOGNIZED_COMMAND;
+            }
+        }
+        static PrepareResult PrepareStatement(string line, Statement statement)
+        {
+            if (line.Substring(0,6) == "insert")
+            {
+                //statement->type = STATEMENT_INSERT;
+                return PrepareResult.PREPARE_SUCCESS;
+            }
+            if (line.Substring(0, 6) == "select")
+            {
+                //statement->type = STATEMENT_SELECT;
+                return PrepareResult.PREPARE_SUCCESS;
+            }
+
+            return PrepareResult.PREPARE_UNRECOGNIZED_STATEMENT;
+        }
         static void Main(string[] args)
         {
             while (true) {
-                Console.WriteLine("What is your name? Type 'exit' to exit");
+                
+                PrintPrompt();
                 string line = Console.ReadLine();
 
-                if (line == "exit") {
-                    Console.WriteLine("Goodbye");
-                    break;
+                if (line[0] == '.')
+                {
+                    switch (DoMetaCommand(line))
+                    {
+                        case MetaCommandResult.META_COMMAND_EXIT:
+                            Console.WriteLine("Goodbye");
+                            return;
+                        case MetaCommandResult.META_COMMAND_SUCCESS:
+                            continue;
+                        case MetaCommandResult.META_COMMAND_UNRECOGNIZED_COMMAND:
+                            Console.WriteLine("Unrecognized command: {0}.", line);
+                            continue;
+                    }
                 }
 
-                string output = String.Format("Hello, {0}", line);
-                Console.WriteLine(output);
+                Statement statement = new Statement();
 
+                switch (PrepareStatement(line, statement))
+                {
+                    case PrepareResult.PREPARE_SUCCESS:
+                        break;
+                    case PrepareResult.PREPARE_UNRECOGNIZED_STATEMENT:
+                        Console.WriteLine("Unrecognized keyword at start of: '{0}'.", line);
+                        continue;
+                }
+
+                statement.ExecuteStatement();
+                Console.WriteLine("Executed.");
             }
         }
     }
